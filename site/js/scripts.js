abea1819f6c011d51bef2435519599bbe557c393
Victor Sz <vicsz@users.noreply.github.com>
2023-04-06 18:58:20 -0400

    Updates.

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Welcome to Dzido's website!");
+
+function getRandomQuote(quotes) {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+function displayQuote(quote) {
+  const quoteTextElement = document.getElementById("quote-text");
+  const quoteAuthorElement = document.getElementById("quote-author");
+
+  quoteTextElement.textContent = quote.text;
+  quoteAuthorElement.textContent = `– ${quote.author}`;
+}
+
+fetch("quotes.json")
+  .then((response) => response.json())
+  .then((quotes) => {
+    const randomQuote = getRandomQuote(quotes);
+    displayQuote(randomQuote);
+  });
